d793bda50b28284ae5653149b939f71800bd0e3b
Angael <krzysztofwidacki@gmail.com>
2021-08-05 20:32:23 +0200

    Working static site.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react'
 import { Root, Routes, addPrefetchExcludes } from 'react-static'
-//
-import { Link, Router } from 'components/Router'
-import Dynamic from 'containers/Dynamic'
+
+import { Router } from 'components/Router'
 
 import './app.css'
 
@@ -12,16 +11,9 @@ addPrefetchExcludes(['dynamic'])
 function App() {
   return (
     <Root>
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/blog">Blog</Link>
-        <Link to="/dynamic">Dynamic</Link>
-      </nav>
       <div className="content">
         <React.Suspense fallback={<em>Loading...</em>}>
           <Router>
-            <Dynamic path="dynamic" />
             <Routes path="*" />
           </Router>
         </React.Suspense>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
+import { Head } from "react-static";
+import TitleCard from "./TitleCard";
 
-export default () => (
-  <div style={{ textAlign: 'center' }}>
-    <h1>Welcome to React-Static</h1>
-  </div>
-)
+const Index = () => (
+  <>
+    <Head>
+      <meta name="theme-color" content="#000000" />
+      <meta
+        name="description"
+        content="Website describing Krzysztof Widacki and his/my projects"
+      />
+      <meta
+        name="keywords"
+        content="Krzysztof, Widacki, React, Frontend, Developer, Programmer, Javascript, Typescript"
+      />
+      <meta name="author" content="Krzysztof Widacki" />
+      <title>Krzysztof Widacki</title>
+    </Head>
+    <TitleCard />
+    <div style={{ textAlign: "center" }}></div>
+  </>
+);
+
+export default Index;
